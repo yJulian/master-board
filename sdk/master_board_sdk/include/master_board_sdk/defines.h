ecf741d59aa152a65cc60273bf9822cfc7fe3b60
Julian Leitner <yjulian@outlook.de>
2025-08-04 15:10:18 +0200

    test

--- sdk/master_board_sdk/include/master_board_sdk/defines.h
+++ sdk/master_board_sdk/include/master_board_sdk/defines.h
@@ -36,13 +36,13 @@ struct imu_packet_t
 	int16_t linear_acceleration[3];
 } __attribute__((packed));
 
-struct imu_data {
-	int16_t accelerometer[3];
-	int16_t magnetometer[3];
-	int16_t gyroscope[3];
-	int16_t attitude[3];
-	int16_t quaternion[4];
-	int16_t linear_acceleration[3];
+struct imu_data_t {
+	float accelerometer[3];
+	float magnetometer[3];
+	float gyroscope[3];
+	float attitude[3];
+	float quaternion[4];
+	float linear_acceleration[3];
 } __attribute__((packed));
 
 struct powerboard_packet_t
--- sdk/master_board_sdk/src/master_board_interface.cpp
+++ sdk/master_board_sdk/src/master_board_interface.cpp
@@ -369,12 +369,12 @@ void MasterBoardInterface::ParseSensorData()
     imu_data.accelerometer[i] = (float)(sensor_packet.imu.accelerometer[i]) * static_cast<float>(0.01);
     imu_data.gyroscope[i] = D16QN_TO_FLOAT(sensor_packet.imu.gyroscope[i], IMU_QN_GYR);
     imu_data.attitude[i] = D16QN_TO_FLOAT(sensor_packet.imu.attitude[i], IMU_QN_EF);
-    imu_data.linear_acceleration[i] = (float)(sensor_packet.imu.linear_acceleration[i]) * static_cast<float>(0.01);<
-    imu_data.magnetometer[i] = D16QN_TO_FLOAT(sensor_packet.magnetometer[i], IMU_QN_MAG);
-    imu_data.quaternion[i] = D16QN_TO_FLOAT(sensor_packet.quaternion[i], IMU_QN_QUAT);
+    imu_data.linear_acceleration[i] = (float)(sensor_packet.imu.linear_acceleration[i]) * static_cast<float>(0.01);
+    imu_data.magnetometer[i] = D16QN_TO_FLOAT(sensor_packet.imu.magnetometer[i], IMU_QN_MAG);
+    imu_data.quaternion[i] = D16QN_TO_FLOAT(sensor_packet.imu.quaternion[i], IMU_QN_QUAT);
   }
   // 4th quaternion packet
-  imu_data.quaternion[3] = D16QN_TO_FLOAT(sensor_packet.quaternion[3], IMU_QN_QUAT);
+  imu_data.quaternion[3] = D16QN_TO_FLOAT(sensor_packet.imu.quaternion[3], IMU_QN_QUAT);
 
   /*Read Power Board data*/
   powerboard_data.current_bus = sensor_packet.powerboard.vshunt * 5e-6f / 4e-3f;
